75ec0a57e6707816ed55e1702c64c2b789aeaf92
newenclave <pikrat.acid@gmail.com>
2017-01-19 17:28:12 +0300

    init

--- main.cpp
+++ main.cpp
@@ -54,8 +54,8 @@ struct byte_order<std::uint16_t, true> {
     typedef std::uint16_t value_type;
     static value_type value( value_type v )
     {
-        return (   v          >> 8 ) |
-               ( ( v & 0xff ) << 8 ) ;
+        return ( ( v & 0xff00 ) >> 8 ) |
+               ( ( v & 0x00ff ) << 8 ) ;
     }
 
 };
@@ -65,10 +65,10 @@ struct byte_order<std::uint32_t, true> {
     typedef std::uint32_t value_type;
     static value_type value( value_type v )
     {
-        return (   v              >> 24 ) |
-               ( ( v & 0xff0000 ) >>  8 ) |
-               ( ( v & 0x00ff00 ) <<  8 ) |
-               ( ( v & 0x0000ff ) << 24 ) ;
+        return ( ( v & 0xff000000 ) >> 24 ) |
+               ( ( v & 0x00ff0000 ) >>  8 ) |
+               ( ( v & 0x0000ff00 ) <<  8 ) |
+               ( ( v & 0x000000ff ) << 24 ) ;
     }
 };
 
@@ -77,14 +77,14 @@ struct byte_order<std::uint64_t, true> {
     typedef std::uint64_t value_type;
     static value_type value( value_type v )
     {
-        return (   v                      >> 56) |
-               ( ( v & 0xff000000000000 ) >> 40) |
-               ( ( v & 0x00ff0000000000 ) >> 24) |
-               ( ( v & 0x0000ff00000000 ) >>  8) |
-               ( ( v & 0x000000ff000000 ) <<  8) |
-               ( ( v & 0x00000000ff0000 ) << 24) |
-               ( ( v & 0x0000000000ff00 ) << 40) |
-               ( ( v & 0x000000000000ff ) << 56) ;
+        return ( ( v & 0xff00000000000000 ) >> 56) |
+               ( ( v & 0x00ff000000000000 ) >> 40) |
+               ( ( v & 0x0000ff0000000000 ) >> 24) |
+               ( ( v & 0x000000ff00000000 ) >>  8) |
+               ( ( v & 0x00000000ff000000 ) <<  8) |
+               ( ( v & 0x0000000000ff0000 ) << 24) |
+               ( ( v & 0x000000000000ff00 ) << 40) |
+               ( ( v & 0x00000000000000ff ) << 56) ;
     }
 };
 
