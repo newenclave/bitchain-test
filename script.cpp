7a1e1eafdccb111b437412096f9759cea0027953
newenclave <pikrat.acid@gmail.com>
2017-08-09 14:10:11 +0300

    scripts

--- script.cpp
+++ script.cpp
@@ -5,6 +5,8 @@
 #include <iostream>
 
 #include "etool/details/byte_order.h"
+#include "etool/details/byte_hex.h"
+
 #include "etool/dumper/dump.h"
 #include "etool/slices/memory.h"
 
@@ -14,6 +16,22 @@
 #include "tx.h"
 
 namespace {
+
+    //return main_script0( );
+    uint8_t priv_bytes[32] = {
+        0x16, 0x26, 0x07, 0x83, 0xe4, 0x0b, 0x16, 0x73,
+        0x16, 0x73, 0x62, 0x2a, 0xc8, 0xa5, 0xb0, 0x45,
+        0xfc, 0x3e, 0xa4, 0xaf, 0x70, 0xf7, 0x27, 0xf3,
+        0xf9, 0xe9, 0x2b, 0xdd, 0x3a, 0x1d, 0xdc, 0x42
+    };
+
+    uint8_t pub_bytes[33] = {
+        0x02,
+        0x82, 0x00, 0x6e, 0x93, 0x98, 0xa6, 0x98, 0x6e,
+        0xda, 0x61, 0xfe, 0x91, 0x67, 0x4c, 0x3a, 0x10,
+        0x8c, 0x39, 0x94, 0x75, 0xbf, 0x1e, 0x73, 0x8f,
+        0x19, 0xdf, 0xc2, 0xdb, 0x11, 0xdb, 0x1d, 0x28
+    };
 
     namespace op {
         enum class code: std::uint8_t {
@@ -93,8 +111,8 @@ namespace {
             bo::write( static_cast<std::uint16_t>(val & 0xFFFF), out );
             std::string res;
             res.push_back( op::to_char(op::code::OP_PUSHDATA2) );
-            res.push_back(out[1]);
             res.push_back(out[0]);
+            res.push_back(out[1]);
 
             return res;
         }
@@ -109,10 +127,10 @@ namespace {
             std::string res;
 
             res.push_back( op::to_char(op::code::OP_PUSHDATA4) );
-            res.push_back(out[3]);
-            res.push_back(out[2]);
-            res.push_back(out[1]);
             res.push_back(out[0]);
+            res.push_back(out[1]);
+            res.push_back(out[2]);
+            res.push_back(out[3]);
 
             return res;
         }
@@ -246,23 +264,90 @@ using namespace etool;
 //88
 //AC
 
+std::string operator "" _bin( const char *val, size_t len )
+{
+    auto res = details::byte_hex::from_hex( val, len );
+    if( res ) {
+        return std::move(*res);
+    }
+    return "<FAILED>";
+}
+
+std::ostream &printhex( std::ostream &out, const std::string &str )
+{
+    auto res = details::byte_hex::to_hex( str );
+    out << *res;
+    return out;
+}
+
+int main_script1( )
+{
+    return 0;
+}
+
+int main_script0( )
+{
+    tx::output      outs[2];
+    tx::output      prev_outs[1];
+    tx::input       ins_sign[1];
+    tx::outpoint    outpoint;
+    tx::transaction tx;
+    std::size_t     msg_len;
+    tx::input       res_tx;
+
+    const char msg_exp[] = "0100000001f3a27f485f9833c8318c490403307f"
+                           "ef1397121b5dd8fe70777236e7371c4ef3000000"
+                           "001976a9146bf19e55f94d986b4640c154d86469"
+                           "934191951188acffffffff02e0fe7e0100000000"
+                           "1976a91418ba14b3682295cb05230e31fecb0008"
+                           "9240660888ace084b003000000001976a9146bf1"
+                           "9e55f94d986b4640c154d86469934191951188ac"
+                           "0000000001000000";
+
+    outs[0].fill( 25100000, "18ba14b3682295cb05230e31fecb000892406608"_bin );
+    outs[1].fill( 61900000, "6bf19e55f94d986b4640c154d864699341919511"_bin );
+    outpoint.fill( "f34e1c37e736727770fed85d1b129713"
+                   "ef7f300304498c31c833985f487fa2f3"_bin, 0 );
+    prev_outs[0].fill( 87000000, "6bf19e55f94d986b4640c154d864699341919511"_bin );
+    ins_sign[0].fill_signable( outpoint, prev_outs[0] );
+
+    tx.tx_out.push_back( outs[0] );
+    tx.tx_out.push_back( outs[1] );
+    tx.tx_in.push_back( ins_sign[0] );
+    tx.locktime = 0;
+    tx.version = 1;
+    msg_len = tx.size( tx::SIGHASH_ALL );
+    std::string res;
+    tx.serialize_to( tx::SIGHASH_ALL, res );
+
+    std::cout << msg_exp << "\n";
+    printhex(std::cout, res) << "\n";
+
+    //res = *details::byte_hex::to_hex( res );
+    auto h = hash::hash256::get_string( res.c_str( ), res.size( ) );
+
+    auto k = crypto::ec_key::create_private( priv_bytes, sizeof(priv_bytes) );
+    auto s = crypto::signature::hash_and_sign( h.c_str( ), h.size( ), k.get( ));
+
+    auto der = s.to_der( k.get( ) );
+
+    tx.tx_in[0].fill( outpoint, der,
+                      std::string(pub_bytes, pub_bytes+sizeof(pub_bytes)),
+                      tx::SIGHASH_ALL );
+
+    res.clear( );
+    tx.serialize_to( tx::SIGHASH_NON, res );
+
+    std::cout << "\n";
+    std::cout << etool::dumper::make<>::to_hex( res.c_str( ), res.size( ), "", " " )
+              << "\n";
+
+    return 0;
+}
+
 int main_script( )
 {
-
-    uint8_t priv_bytes[32] = {
-        0x16, 0x26, 0x07, 0x83, 0xe4, 0x0b, 0x16, 0x73,
-        0x16, 0x73, 0x62, 0x2a, 0xc8, 0xa5, 0xb0, 0x45,
-        0xfc, 0x3e, 0xa4, 0xaf, 0x70, 0xf7, 0x27, 0xf3,
-        0xf9, 0xe9, 0x2b, 0xdd, 0x3a, 0x1d, 0xdc, 0x42
-    };
-
-    uint8_t pub_bytes[33] = {
-        0x02,
-        0x82, 0x00, 0x6e, 0x93, 0x98, 0xa6, 0x98, 0x6e,
-        0xda, 0x61, 0xfe, 0x91, 0x67, 0x4c, 0x3a, 0x10,
-        0x8c, 0x39, 0x94, 0x75, 0xbf, 0x1e, 0x73, 0x8f,
-        0x19, 0xdf, 0xc2, 0xdb, 0x11, 0xdb, 0x1d, 0x28
-    };
+    return main_script0( );
 
     const std::string message = "This is a very confidential message\n";
     auto k  = crypto::ec_key::create_private( priv_bytes, sizeof(priv_bytes) );
@@ -280,3 +365,50 @@ int main_script( )
 
     return 0;
 }
+
+/*
+01 00 00 00
+01
+
+F3 A2 7F 48 5F 98 33 C8
+31 8C 49 04 03 30 7F EF
+13 97 12 1B 5D D8 FE 70
+77 72 36 E7 37 1C 4E F3
+
+00 00 00 00
+
+6B 48 30 45 02 21 00 D5
+A5 5F 3E 18 2B D7 4C 97
+4A AE 33 85 B6 CD AC A9
+9F 3F A9 F6 3F E6 E9 85
+46 D2 AF 86 38 0B E6 02
+20 7E 8F 37 B2 21 B1 32
+48 EA A4 A6 13 7E 1B 52
+A6 2F C2 D5 1F E2 8B C5
+38 83 11 D0 EF BE 69 AD
+60 01 21 02 82 00 6E 93
+98 A6 98 6E DA 61 FE 91
+67 4C 3A 10 8C 39 94 75
+BF 1E 73 8F 19 DF C2 DB
+11 DB 1D 28
+
+FF FF FF FF
+
+02
+
+E0 FE 7E 01 00 00 00 00
+
+19 76 A9 14 18 BA 14 B3
+68 22 95 CB 05 23 0E 31
+FE CB 00 08 92 40 66 08
+88 AC
+
+E0 84 B0 03 00 00 00 00
+
+19 76 A9 14 6B F1 9E 55
+F9 4D 98 6B 46 40 C1 54
+D8 64 69 93 41 91 95 11
+88 AC
+
+00 00 00 00
+*/
--- tx.h
+++ tx.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <deque>
 #include <array>
+#include <algorithm>
 
 #include "etool/details/byte_order.h"
 #include "etool/sizepack/blockchain_varint.h"
@@ -13,7 +14,23 @@
 namespace bchain { namespace tx {
 
     enum sighash {
+        SIGHASH_NON = 0,
         SIGHASH_ALL = 1,
+    };
+
+    struct order {
+        template <typename T>
+        using little = etool::details::byte_order_little<T>;
+
+        template <typename T>
+        static
+        void reverse( T *begin, std::size_t len )
+        {
+            auto middle = len / 2;
+            for( std::size_t i =0; i<middle; ++i ) {
+                std::swap(begin[i], begin[len - i - 1]);
+            }
+        }
     };
 
     struct ser {
@@ -28,7 +45,7 @@ namespace bchain { namespace tx {
         static
         void append32( std::uint32_t val, std::string &out )
         {
-            using bo32 = etool::details::byte_order_little<std::uint32_t>;
+            using bo32 = order::little<std::uint32_t>;
             auto s = out.size( );
             out.resize( s + sizeof(std::uint32_t) );
             bo32::write( val, &out[s] );
@@ -37,7 +54,7 @@ namespace bchain { namespace tx {
         static
         void append64( std::uint64_t val, std::string &out )
         {
-            using bo64 = etool::details::byte_order_little<std::uint64_t>;
+            using bo64 = order::little<std::uint64_t>;
             auto s = out.size( );
             out.resize( s + sizeof(std::uint64_t) );
             bo64::write( val, &out[s] );
@@ -47,11 +64,12 @@ namespace bchain { namespace tx {
         void append_var( std::uint64_t val, std::string &out )
         {
             using bo_var = etool::sizepack::blockchain_varint;
-            bo_var::pack( val, out );
+            bo_var::append( val, out );
         }
     };
 
     struct output {
+
         std::uint64_t value;
         std::vector<std::uint8_t> script;
 
@@ -62,15 +80,31 @@ namespace bchain { namespace tx {
             res += script.size( );
             return res;
         }
+
         void serialize_to( std::string &out ) const
         {
             ser::append64( value, out );
             ser::append_var( script.size( ), out );
             out.append( script.begin( ), script.end( ) );
         }
+
+        void fill( std::uint64_t val, const std::string &hash160 )
+        {
+            std::vector<std::uint8_t> tmp;
+            tmp.push_back( 0x76 );
+            tmp.push_back( 0xa9 );
+            tmp.push_back( 0x14 );
+            tmp.insert( tmp.end( ), hash160.begin( ), hash160.end( ) );
+            tmp.push_back( 0x88 );
+            tmp.push_back( 0xac );
+
+            script.swap(tmp);
+            value = val;
+        }
     };
 
     struct outpoint {
+
         std::array<std::uint8_t, 32> txid; /// hash256
         std::uint32_t index;
 
@@ -79,6 +113,19 @@ namespace bchain { namespace tx {
             std::size_t res = 32;
             res += sizeof( index );
             return res;
+        }
+
+        void serialize_to( std::string &out ) const
+        {
+            out.append( txid.begin( ), txid.end( ) );
+            ser::append32( index, out );
+        }
+
+        void fill( const std::string &tid, std::uint32_t idx )
+        {
+            std::copy( tid.begin( ), tid.end( ), txid.begin( ) );
+            order::reverse( txid.data( ), txid.size( ) );
+            index = idx;
         }
     };
 
@@ -98,20 +145,48 @@ namespace bchain { namespace tx {
 
         void serialize_to( std::string &out ) const
         {
-            out.append( op.txid.begin( ), op.txid.end( ) );
-            ser::append32( op.index, out );
-            ser::append32( static_cast<std::uint32_t>(script.size( )), out );
+            op.serialize_to( out );
+            ser::append_var( script.size( ), out );
             out.append( script.begin( ), script.end( ) );
             ser::append32( seq, out );
+        }
+
+        void fill( outpoint out, const std::string &sig,
+                   const std::string &pub, sighash flag )
+        {
+            std::vector<std::uint8_t> tmp;
+            tmp.push_back( static_cast<std::uint8_t>(sig.size( ) + 1) );
+            tmp.insert( tmp.end( ), sig.begin( ), sig.end( ) );
+            tmp.push_back( static_cast<std::uint8_t>(flag) );
+            tmp.push_back( static_cast<std::uint8_t>(pub.size( ) ) );
+            tmp.insert( tmp.end( ), pub.begin( ), pub.end( ) );
+
+            script.swap( tmp );
+            op = std::move(out);
+            seq = 0xffffffff;
+        }
+
+        void fill_signable( outpoint opoint, output oput )
+        {
+            op = std::move(opoint);
+            script.swap( oput.script );
+            seq = 0xffffffff;
+        }
+
+        void fill_truncated( outpoint opoint )
+        {
+            op = std::move(opoint);
+            script.clear( );
+            seq = 0xffffffff;
         }
     };
 
     struct transaction {
 
-        std::uint32_t version = 1;
-        std::deque<input>  tx_in;
-        std::deque<output> tx_out;
-        std::uint32_t locktype = 0;
+        std::uint32_t       version = 1;
+        std::deque<input>   tx_in;
+        std::deque<output>  tx_out;
+        std::uint32_t       locktime = 0;
 
         std::size_t size( sighash flags ) const
         {
@@ -128,7 +203,7 @@ namespace bchain { namespace tx {
                 res += o.size( );
             }
 
-            res += sizeof(locktype);
+            res += sizeof(locktime);
 
             if( flags ) {
                 res += sizeof(std::uint32_t);
@@ -151,7 +226,7 @@ namespace bchain { namespace tx {
                 o.serialize_to( out );
             }
 
-            ser::append32( locktype, out );
+            ser::append32( locktime, out );
 
             if( flags ) {
                 ser::append32( flags, out );
